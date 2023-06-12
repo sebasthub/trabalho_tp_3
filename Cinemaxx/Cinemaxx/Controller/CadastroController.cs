caaeaed64d9e049316d85b7092c38061817109cd
Beatriz Angélica <92961502+angelicabeatriz@users.noreply.github.com>
2023-06-12 19:42:18 -0300

    Tela Cadastro

---
+++ Cinemaxx/Cinemaxx/Controller/CadastroController.cs
@@ -0,0 +1,24 @@
+﻿using Cinemaxx.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Cinemaxx.Controller
+{
+    public class CadastroController : ControllerContext
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Salvar (CadastroViewModel cadastro)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Sucesso");
+            }
+
+            return View("Cadastro", cadastro);
+        }
+    }
+}
---
+++ Cinemaxx/Cinemaxx/Model/CadastroViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinemaxx.ViewModel
+{
+    public class CadastroViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; } 
+        public string Senha { get; set; }
+
+    }
+}
