328c648af6d7c161c55915b3b5d330b0ba462953
Beatriz Angélica <92961502+angelicabeatriz@users.noreply.github.com>
2023-06-15 21:22:08 -0300

    Tela de Programação

---
+++ Cinemaxx/Pages/Programacao/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cinemaxx.Pages.Programacao
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
